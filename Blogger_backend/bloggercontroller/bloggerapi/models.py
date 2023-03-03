ae2abe8a13d973d823eaa919064c0d044585cf77
Rodel <lentodowner@gmail.com>
2023-03-03 03:23:48 -0500

    API model complete

--- Blogger_backend/bloggercontroller/bloggerapi/models.py
+++ Blogger_backend/bloggercontroller/bloggerapi/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class VideoTranscribe(models.Model):
+    id = models.DecimalField(max_digits="10", unique=True)
+    title = models.CharField(max_length=255, unique=True)
+    videoTranscribe = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    video_file = models.FileField(upload_to='videos/')
