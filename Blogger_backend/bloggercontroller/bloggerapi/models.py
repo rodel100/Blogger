a0e535a47c2772262ddbac9c4615f6dac3f7d168
Rodel <lentodowner@gmail.com>
2023-03-03 03:42:20 -0500

    Models update

--- Blogger_backend/bloggercontroller/bloggerapi/models.py
+++ Blogger_backend/bloggercontroller/bloggerapi/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class VideoTranscribe(models.Model):
-    code = models.CharField(max_length=8)
+    code = models.CharField(max_length=8, unique=True)
     title = models.CharField(max_length=255, unique=True)
     videoTranscribe = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
