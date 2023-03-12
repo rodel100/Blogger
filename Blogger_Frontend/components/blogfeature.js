60e367c6db308e503df14c5b87ea23112cbc1388
Rodel <lentodowner@gmail.com>
2023-03-12 07:13:30 -0400

    Select file added

--- Blogger_Frontend/components/blogfeature.js
+++ Blogger_Frontend/components/blogfeature.js
@@ -1,8 +1,24 @@
+import { useCallback, useEffect, useState, } from "react"
+import { View, style, Button, Text } from "react-native";
+import * as DocumentPicker from 'expo-document-picker';
+
 export default function Blogger() {
+  let [file, setFile] = useState([]);
+  const documentSelector = useCallback(async () => {
+    try {
+      const x = await DocumentPicker.getDocumentAsync();
+      setFile(x);
+      console.log(file);
+    }
+    catch(err){
+      console.log(err)
+    }
+  },[])
     return (
-      <View style={styles.container}>
+      <View >
+                <Button title="log File" onPress={()=> {console.log(file)}}/>
         <Text>Input your video or mp3 file</Text>
-        <StatusBar style="auto" />
+        <Button title="Select File" onPress={documentSelector}/>
       </View>
     )
 }
