30032d4072bbf2010b0f0f4502d62babfc0c6eb4
Rodel <lentodowner@gmail.com>
2023-03-11 20:01:06 -0500

    BloggerFeatureStarted

---
+++ Blogger_Frontend/components/blogfeature.js
@@ -0,0 +1,8 @@
+export default function Blogger() {
+    return (
+      <View style={styles.container}>
+        <Text>Input your video or mp3 file</Text>
+        <StatusBar style="auto" />
+      </View>
+    )
+}
