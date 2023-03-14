e0da222d08e773d827afd6753dd6203c67bf21b3
Rodel <lentodowner@gmail.com>
2023-03-14 09:59:29 -0400

    Submit button begun

--- Blogger_Frontend/components/blogfeature.js
+++ Blogger_Frontend/components/blogfeature.js
@@ -1,9 +1,12 @@
 import { useCallback, useEffect, useState, } from "react"
 import { View, style, Button, Text } from "react-native";
 import * as DocumentPicker from 'expo-document-picker';
+import { TextInput } from "react-native-web";
 
 export default function Blogger() {
   let [file, setFile] = useState([]);
+  let [title, setTitle] = useState("");
+  const formData = new FormData();
   const documentSelector = useCallback(async () => {
     try {
       const x = await DocumentPicker.getDocumentAsync();
@@ -14,9 +17,14 @@ export default function Blogger() {
       console.log(err)
     }
   },[])
+  const submitButton = () => {
+    formData.append("title", title)
+    formData.append("video_file", file[0])
+  }
     return (
       <View >
-                <Button title="log File" onPress={()=> {console.log(file)}}/>
+        <Text>{title}</Text>
+        <TextInput onChangeText={(text) => setTitle(text)}></TextInput>
         <Text>Input your video or mp3 file</Text>
         <Button title="Select File" onPress={documentSelector}/>
       </View>
