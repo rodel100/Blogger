b867a59346657ac2399418886d56add94d310c9f
Rodel <lentodowner@gmail.com>
2023-04-11 06:39:01 -0400

    SomeCSS added

--- Blogger_Frontend/components/GetVideo.jsx
+++ Blogger_Frontend/components/GetVideo.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import {ShowVideo} from './ShowVideo'
 
 function GrabVideo() {
     const [title, setTitle] = useState('')
@@ -8,11 +9,12 @@ function GrabVideo() {
         fetch('http://localhost:3000/api/upload', {
             method: 'POST',
             mode: 'cors',
-            
+
             body: JSON.stringify({title,video_file})})}
 
   return(
     <div>
+        <ShowVideo file={file} />
         <div>Enter Title and File</div>
         <input type="text" placeholder="Title" onChange={(e) => setTitle(e.target.value)} />
         <input type={"file"} onChange={(e) => setFile(e.target.files[0])} />
